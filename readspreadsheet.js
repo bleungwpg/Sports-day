e4d62e3ed8925e9551a1854456c1d39b7a9ef42c
bleungwpg <benjamin.leung80@gmail.com>
2017-01-03 14:53:13 +0800

    2017 version

---
+++ readspreadsheet.js
@@ -0,0 +1,155 @@
+google.load("visualization", "1", {packages:["corechart"]});
+
+var alldata = [[]];
+var filterdata = [[]];
+var maxRows;
+var maxCols;
+var filterRows;
+var inp;
+var startGapSize = 25;
+var continuousGapSize = 25;
+var spreadsheetID;
+var sheetID;
+var wholehyperlink;
+var testing;
+var dataSubmitted;
+
+
+
+
+function showData()
+{
+
+
+      background(0,0,0);
+	  fill(255,255,255);
+	  
+	  moreH = 125;
+	  
+      text("Student ID",25+moreH,25);
+      text("Last Name",100+moreH,25);
+      text("First Name",200+moreH,25);
+
+      for (var r = 0; r < maxRows; r++)
+      {
+        text(r,370+moreH,(r+1)*continuousGapSize+startGapSize);
+        for (var c = 0; c < maxCols; c++)
+        {
+          if (c == 0)
+          {
+            text(alldata[r][c],25+moreH,(r+1)*continuousGapSize+startGapSize);
+          }
+          if (c == 1)
+          {
+            text(alldata[r][c],100+moreH,(r+1)*continuousGapSize+startGapSize);
+          }
+          if (c == 2)
+          {
+            text(alldata[r][c],200+moreH,(r+1)*continuousGapSize+startGapSize);
+          }
+        }
+      }
+}
+
+
+function pullData()
+{
+	// parse sheet ID and tab ID
+	wholehyperlink = urlinp.value();
+	startID = wholehyperlink.search("spreadsheets/d/");
+	spreadsheetID = wholehyperlink.substring(startID+15,startID+65);
+	spreadsheetID = spreadsheetID.substring(0,spreadsheetID.indexOf("/"));
+	
+	startID = wholehyperlink.search("gid=");
+	sheetID = wholehyperlink.substring(startID+4,wholehyperlink.length);
+
+	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/'+spreadsheetID+'/gviz/tq?sheet=Grade6'+'&tq=SELECT*');
+//	var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1CGxETFmlqy4lYYhtWcIMd7COtW9MR11EN0YI47XcvCk/gviz/tq?sheet=Grade6&tq=SELECT*');
+
+
+    query.send(handleQueryResponse);
+}
+
+// Called when the query response is returned.
+function handleQueryResponse(response) {
+
+	if (testing == true)
+	{
+		console.log("2 - Hello World!");
+	}
+	
+	if (response.isError()) {
+		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return;
+	}
+
+
+	var data = response.getDataTable();
+
+	maxRows = data.getNumberOfRows();
+	maxCols = data.getNumberOfColumns();
+
+
+	if (testing == true)
+	{
+		console.log('There are '+maxRows+' rows');
+		console.log('There are '+maxCols+' cols');	
+	}
+
+
+	var rowdata = new Array(maxCols);
+	alldata = new Array(maxRows);
+	filterRows = 0;
+
+	for (var r = 0; r < maxRows; r++)
+	{
+		alldata[r] = new Array(maxCols);
+		for (var c = 0; c < maxCols; c++)
+		{
+			var info = data.getValue(r,c);
+			if (info == null)
+			{
+				alldata[r][c] = "";
+			}
+			else
+			{
+				alldata[r][c] = info;	
+			}
+			
+			// if they are ribbon winners add one to count
+			if (alldata[r][8] == 1 || alldata[r][8] == 2)
+			{
+				filterRows++;
+			}
+		}
+	}
+	
+
+	filterdata = new Array(filterRows);
+	
+	filterRows = 0;
+	// copy only the ribbon winner data
+	for (var r = 0; r < maxRows; r++)
+	{
+		if (alldata[r][8] == 1 || alldata[r][8] == 2)
+		{
+			filterdata[filterRows] = new Array(maxCols);
+		
+			for (var c = 0; c < maxCols; c++)
+			{
+				filterdata[filterRows][c] = alldata[r][c];	
+			}
+			filterRows++;
+		}
+	}
+	
+	
+
+	// allow data to be shown after it has been submitted
+	dataSubmitted = true;
+
+	// hide textbox and button
+	urlinp.hide();
+	submitbutton.hide();
+
+}
---
+++ sketch.js
@@ -0,0 +1,309 @@
+var singlePrime;
+var randomIndex;
+var thimble;
+var uniqueOn;
+var markUniqueStudent;
+var currentGrade;
+var irow;
+var formData;
+
+
+function setup(){
+	//create a drawing service 500px wide, 500px tall
+	createCanvas(1053,1505);
+
+	thimble = false;
+	testing = true;
+	dataSubmitted = false;
+	uniqueOn = false;
+	setupRandom();
+	singlePrime = false;
+	reset = false;
+	first = true;
+	once = true;
+	currentGrade = 6;
+	irow = 0;
+
+	// load up all textboxes and buttons in the interface
+	urlinp = createInput();
+	urlinp.position(10,40);
+	urlinp.size(300,25);  
+	submitbutton = createButton('submit');
+	submitbutton.position(400, 105);
+	submitbutton.mousePressed(pullData);
+	
+	checkbox = createCheckbox();
+	if (thimble == true)
+	{
+		checkbox.position(18,92);
+	}
+	else
+	{
+		checkbox.position(15,490);
+	}
+	checkbox.changed(checkUniqueMode);
+	checkbox.hide();
+	
+
+	
+	if (testing == true)
+	{
+		wholehyperlink = "https://docs.google.com/spreadsheets/d/1CGxETFmlqy4lYYhtWcIMd7COtW9MR11EN0YI47XcvCk/edit#gid=1021250630";
+		
+		startID = wholehyperlink.search("spreadsheets/d/");
+		spreadsheetID = wholehyperlink.substring(startID+15,startID+65);
+		spreadsheetID = spreadsheetID.substring(0,spreadsheetID.indexOf("/"));
+	
+		startID = wholehyperlink.search("gid=");
+		sheetID = wholehyperlink.substring(startID+4,wholehyperlink.length);
+
+
+		// update notes: https://docs.google.com/spreadsheets/d/{SHEETID}/gviz/tq?sheet={SHEET NAME}&tq=SELECT*
+//		var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1CGxETFmlqy4lYYhtWcIMd7COtW9MR11EN0YI47XcvCk/gviz/tq?sheet=Grade6&tq=SELECT*');
+		var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/'+spreadsheetID+'/gviz/tq?sheet=Grade6'+'&tq=SELECT*');
+
+		query.send(handleQueryResponse);
+	}
+}
+
+function draw(){
+
+	if (testing == true && dataSubmitted == true)
+	{
+		urlinp.hide();
+		submitbutton.hide();
+	}
+	
+	if (dataSubmitted == true)
+	{
+		showData();
+	}
+	else
+	{
+		background(255,255,255);
+		// Prompt to enter your spreadsheet URL
+		fill(0,0,0);
+		if (thimble == true)
+		{
+			text("Enter your spreadsheet URL",10,13);
+		}
+		else
+		{
+			text("Enter your spreadsheet URL",10,33);
+		}
+
+		if (testing == true)
+		{
+			text("URL: "+wholehyperlink,10,300);
+			text("spreadsheetID: "+spreadsheetID,10,325);
+			text("sheetID: "+sheetID,10,350);
+		}
+	}
+}
+
+
+
+function setupRandom()
+{
+	// do not allow random generation
+	startRandom = false;
+	singlePrime = false;
+	lock = false;
+	randomIndex = 0;
+}
+
+
+// Create buttons from grades 6 - 12
+function drawButtons()
+{
+	buttonGap = 60;
+
+	for (var b = 0; b < 7; b++)
+	{
+		if (currentGrade-6 == b)
+		{
+			fill(0,255,0);		
+		}
+		else
+		{
+			fill(255,255,255);
+		}
+		rect(10,10+buttonGap*b,100,50);
+		fill(0,0,0);
+		text("Grade "+(b+6),34,39+buttonGap*b);
+		
+		if (mouseIsPressed && mouseX > 10 && mouseX < 10+100 &&
+		    mouseY > 10+buttonGap*b && mouseY < 10+buttonGap*b+50)
+		{
+			currentGrade = b+6;
+			var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/'+spreadsheetID+'/gviz/tq?sheet=Grade'+currentGrade+'&tq=SELECT*');
+
+			query.send(handleQueryResponse);
+		}
+	}
+
+}
+
+function showData()
+{
+	background(0,0,0);
+	
+	// draw button
+	drawButtons();
+	
+	// show checkbox
+	checkbox.show();
+	fill(255,255,255);
+	text("Unique mode",32,504);
+
+	
+
+	// show data
+	col1 = 25;
+	col2 = 170;
+	col3 = 250;
+	col4 = 340;
+	col5 = 410;
+	col6 = 490;
+	col7 = 550;
+
+	fill(255,255,255);
+	moreH = 125;
+	text("Name",col1+moreH,25);
+	text("Student ID",col2+moreH,25);
+	text("House",col3+moreH,25);
+	text("Event",col4+moreH,25);
+	text("Gender",col5+moreH,25);
+	text("Place",col6+moreH,25);
+	text("Ribbon",col7+moreH,25);
+
+	irow = 0;
+	for (var r = 0; r < filterRows; r++)
+	{
+		hoverOverGapSize = 8;
+		// highlight where the mouse hovering over with a green background
+		if (mouseY > (r)*continuousGapSize+startGapSize+hoverOverGapSize && 
+		    mouseY < (r+1)*continuousGapSize+startGapSize+hoverOverGapSize && mouseX > 140 && mouseX < 140+580)
+		{
+			fill(0,255,50);
+			rect(140,(r)*continuousGapSize+startGapSize+hoverOverGapSize,580,25);
+			fill(0,0,0);
+			
+			// if mouse is pressed toggle the status
+			if (mouseIsPressed && lock == false)
+			{
+				if (filterdata[r][8] == 1)
+				{
+					filterdata[r][8] = 2;
+					lock = true;
+				}
+				else
+				{
+					filterdata[r][8] = 1;
+					lock = true;
+				}
+				
+				// start - submit the data back to the spreadsheet
+				formData = new FormData();
+				formData.append("Sheet Name","Grade"+currentGrade);
+				formData.append("Student ID",filterdata[r][1]);
+				formData.append("Event",filterdata[r][3]);
+				formData.append("Ribbon Status",filterdata[r][8]);
+
+				var request = new XMLHttpRequest();
+
+				// MARK - MUST CHANGE 2
+				request.open("POST", "https://script.google.com/macros/s/AKfycbwoePIQmE3KMtgAlzcyh93OHSDZhJPlyvl_4T7jAp2Zfb-qmmY/exec");
+				request.send(formData);
+				// end - submit data back to the spreadsheet
+			}
+		}
+		else
+		{
+			fill(255,255,255);
+		}
+
+
+		// populate data
+		for (var c = 0; c < maxCols; c++)
+		{
+			if (c == 0)
+			{
+				text(filterdata[r][c],col1+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 1)
+			{
+				text(filterdata[r][c],col2+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 2)
+			{
+				text(filterdata[r][c],col3+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 3)
+			{
+				text(filterdata[r][c],col4+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 4)
+			{
+				text(filterdata[r][c],col5+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 5)
+			{
+				text(filterdata[r][c],col6+moreH,(r+1)*continuousGapSize+startGapSize);
+			}
+			if (c == 8)
+			{
+				if (filterdata[r][c] == 1)
+				{
+					text("Earned",col7+moreH,(r+1)*continuousGapSize+startGapSize);
+				}
+				else if (filterdata[r][c] == 2)
+				{
+					text("Given",col7+moreH,(r+1)*continuousGapSize+startGapSize);
+				}
+			}
+		}
+	}
+
+}
+
+function checkUniqueMode() {
+	if (this.checked()) {
+		markUniqueStudent = new Array(maxRows);
+		for (var i = 0; i < maxRows; i++)
+		{
+			markUniqueStudent[i] = 0;
+		}
+		uniqueOn = true;
+		first = true;
+		console.log("Checking!");
+	}
+	else {
+		uniqueOn = false;
+		first = true;
+		console.log("Unchecking!");
+	}
+}
+
+function mouseReleased()
+{
+	lock = false;
+}
+
+function submitform()
+{
+	formData.append("Sheet Name","333");
+	formData.append("Student ID","java");
+	formData.append("Event","demo");
+	formData.append("Ribbon Status","hi@gmail.com");
+
+
+	var request = new XMLHttpRequest();
+	
+	
+	// MARK - MUST CHANGE 2
+	request.open("POST", "https://script.google.com/macros/s/AKfycbwoePIQmE3KMtgAlzcyh93OHSDZhJPlyvl_4T7jAp2Zfb-qmmY/exec");		
+						  
+	
+	request.send(formData);
+}
